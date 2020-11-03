8911329203a2f12e5f6308be8b4f2adeb88e5c64
Carlos Sáenz Adán <carlos@MacBook-Pro-de-Carlos.local>
2020-11-03 13:38:18 +0100

    first commit

---
+++ src/main/java/org/jesuitasrioja/ftpproject/App.java
@@ -0,0 +1,13 @@
+package org.jesuitasrioja.ftpproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
