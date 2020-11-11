97441e0c1efa28a83030bc5aee2d6c75218f3ca5
Marcos Asensio <60777608+MKS2508@users.noreply.github.com>
2020-11-11 21:17:03 +0100

    Update App.java

--- src/main/java/org/jesuitasrioja/ftpproject/App.java
+++ src/main/java/org/jesuitasrioja/ftpproject/App.java
@@ -1,15 +1,62 @@
 package org.jesuitasrioja.ftpproject;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.SocketException;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
+import org.jesuitasrioja.ftpproject.ftpLayer.FTPOperations;
 import org.jesuitasrioja.ftpproject.ftpLayer.FTPOperationsImpl;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
 
-    }
+	public static void main(String[] args) {
+
+		FTPOperationsImpl ftp = new FTPOperationsImpl();
+
+		System.out.print(ftp.directorioDeTrabajo());
+		System.out.println();
+		System.out.println();
+		System.out.println("---------------------------------------------------------------------");
+		ftp.cambiarDirectorioDeTrabajo("./apuntesPSP"); // ademas de devolver boolean ejecuta pwd y lo muestra
+		System.out.println();
+		System.out.println();
+		System.out.println("---------------------------------------------------------------------");
+		ftp.subirFichero("./logo.jpeg");
+		System.out.println();
+		System.out.println();
+		System.out.println("---------------------------------------------------------------------");
+		ftp.bajarFichero("logo.jpeg", "./");
+		System.out.println();
+		System.out.println();
+		System.out.println("---------------------------------------------------------------------");
+		ftp.eliminarFichero("logo.jpeg");
+		System.out.println();
+		System.out.println();
+		System.out.println("---------------------------------------------------------------------");
+		ftp.crearCarpeta("./apuntesPSP24");
+		System.out.println();
+		System.out.println();
+		System.out.println("---------------------------------------------------------------------");
+		ftp.eliminarCarpeta("./apuntesPSP24");
+		System.out.println();
+		System.out.println();
+		System.out.println("---------------------------------------------------------------------");
+		System.out.println(ftp.listaFicherosCarpeta("./"));
+		System.out.println();
+		System.out.println();
+		System.out.println("---------------------------------------------------------------------");
+		System.out.println(ftp.listaDirectoriosCarpeta("./"));
+		System.out.println();
+		System.out.println();
+		System.out.println("---------------------------------------------------------------------");
+//		ftp.mostrarInformacionFile(FTPFile); // falta pasar como parametro FTPFILE
+		System.out.println();
+		System.out.println();
+		System.out.println("---------------------------------------------------------------------");
+
+	}
 }
